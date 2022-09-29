9f4975ef82bca0115e9da2689c50ba3226bc6fc8
Amazingdann <alvarezvaldaniel@hotmail.com>
2022-09-29 16:34:37 -0500

    función agregartarea

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
 console.log("Entro al main.js");
+const tareaNombre = document.getElementById("tareaNombre");
+const tareaDetalle = document.getElementById("tareaDetalle");
+const lista = document.getElementById("lista");
+const tareas = [ 
+    {
+        tarea:tareaNombre,
+        detalle: tareaDetalle,
+    }
+]
+
+function agregarTarea() {
+    if(tareaNombre === "") {
+        alert("Ingresa una tarea o no molestes");
+    }
+    else {
+        const Tarea = tareaNombre.value;
+        const Detalle= tareaDetalle.value
+        console.log(Tarea)
+        console.log (Detalle)
+        localStorage.setItem("Tarea", JSON.stringify(Tarea));
+        localStorage.setItem("Detalle", JSON.stringify(Detalle));
+        tareaDetalle.value = "";
+    }
+    };
+
+function actualizarLista(){
+
+};
+
+function tareaCumplida (){
+
+};
+function eliminarTarea(){
+
+;}
