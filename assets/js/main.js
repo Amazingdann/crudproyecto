b8774525908967c111e220c976a70e0c580dbb2d
Amazingdann <alvarezvaldaniel@hotmail.com>
2022-10-19 17:29:43 -0500

    función actualizar lista completada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,10 @@
 console.log("Entro al main.js");
 
 let tareaNombre = document.getElementById("tareaNombre");
-let tareaDetalle = document.getElementById("tareaDetalle");
-const tareasPendientes = document.getElementById("tareasPendientes");
-const elementoLista = document.getElementById("elementoLista");
-const detalleLista = document.getElementById("detalleLista");
+let tareasPendientes = document.getElementById("tareasPendientes");
 let tareas = (localStorage.getItem("tareas")) ? JSON.parse(localStorage.getItem("tareas")): [
 ];
-actualizarLista()
-
+actualizarLista();
 // investigar if ternario  
 ////{tarea:tareaNombre.value, detalle: tareaDetalle.value}
 
@@ -18,28 +14,52 @@ function agregarTarea() {
         alert("Ingresa una tarea maldito insecto");
     }
     else {
+        console.log(tareaNombre.value)
         const tarea = tareaNombre.value;
-        const detalle = tareaDetalle.value; 
-        tareas.push(tarea, detalle)
+        tareas.push(tarea);
         console.log (tarea);
-        console.log (detalle);
-        localStorage.setItem("tareas", JSON.stringify(tareas))
-        console.log(tareas);
+        localStorage.setItem("tareas", JSON.stringify(tareas));
+        actualizarLista();
     }
     };
-
-
 function actualizarLista(){
-    if(tareas.lenght === 0){
-    tareasPendientes.innerHTML= `<li class="list-group-item d-flex align-items-center">
-    No hay Tareas Pendientes
-</li>`;
-    }
-    else {
+    console.log(tareas);
+         if(tareas.length === 0){ 
+            console.log("no hay tareas");
+            tareasPendientes.innerHTML= `<li class="list-group-item d-flex align-items-center"> No Hay Tareas Pendientes </li>`;
+            }
+            else{ 
+            console.log("hay tareas pendientes");
+            tareasPendientes.innerHTML = "";
+            for(const tareaNombre of tareas){
+                console.log("entró creación de lista")
+                const li = document.createElement("li");
+                li.innerText = tareaNombre;
+                li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center", "glass");
 
-    }
-};
+                const divbuttons = document.createElement("div");
+                divbuttons.classList.add("text-align-right")
 
+                const botoncheck = document.createElement("button");
+                botoncheck.classList.add("btn", "btn-success", "btn-sm")
+                botoncheck.innerText = "✓"
+
+                const botoncross = document.createElement("button");
+                botoncross.innerText = " X ";
+                botoncross.classList.add("btn", "btn-danger", "btn-sm")
+                
+                divbuttons.appendChild (botoncheck);
+                divbuttons.appendChild (botoncross);
+                li.appendChild(divbuttons);
+                tareasPendientes.appendChild(li);
+                }
+            }
+    }
+    
+    
+
+
+ 
 
 function tareaCumplida (){
 
